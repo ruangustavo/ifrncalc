396dbb0aa2cc63a40b287db37118040e07d7778a
Ruan Gustavo <ruan.gustavo@escolar.ifrn.edu.br>
2023-12-08 11:26:27 -0300

    refactor: refactor grade calculation and response format

--- src/app/api/grades/route.ts
+++ src/app/api/grades/route.ts
@@ -61,6 +61,7 @@ export async function GET() {
     )
     .then((response) => response.data);
 
+  const grades = [];
   for (let i = 0; i < response.length; i++) {
     const discipline = response[i];
 
@@ -74,12 +75,30 @@ export async function GET() {
       discipline.quantidade_avaliacoes
     );
 
-    response[i] = {
-      ...discipline,
-      disciplina: parseDisciplineName(discipline.disciplina),
-      nota_para_passar: gradeToPass,
-    };
+    const isAvailable = (grade: number | null, index: number) =>
+      grade === null && index <= discipline.quantidade_avaliacoes;
+
+    grades.push({
+      name: parseDisciplineName(discipline.disciplina),
+      E1: {
+        grade: discipline.nota_etapa_1.nota,
+        isAvailable: isAvailable(discipline.nota_etapa_1.nota, 1),
+      },
+      E2: {
+        grade: discipline.nota_etapa_2.nota,
+        isAvailable: isAvailable(discipline.nota_etapa_2.nota, 2),
+      },
+      E3: {
+        grade: discipline.nota_etapa_3.nota,
+        isAvailable: isAvailable(discipline.nota_etapa_3.nota, 3),
+      },
+      E4: {
+        grade: discipline.nota_etapa_4.nota,
+        isAvailable: isAvailable(discipline.nota_etapa_4.nota, 4),
+      },
+      passingGrade: gradeToPass,
+    });
   }
 
-  return NextResponse.json(response);
+  return NextResponse.json(grades);
 }
