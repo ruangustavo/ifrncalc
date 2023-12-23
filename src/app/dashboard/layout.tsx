efc5795271b8e68d8d55da277cfb9422ae592318
Ruan Gustavo <ruan.gustavo@escolar.ifrn.edu.br>
2023-12-23 11:23:09 -0300

    feat: add dashboard layout component

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,18 @@
+import { Metadata } from 'next'
+import React from 'react'
+import { Header } from './_components/header'
+
+export const metadata: Metadata = { title: 'Boletim' }
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <>
+      <Header />
+      {children}
+    </>
+  )
+}
